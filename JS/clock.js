79fcd1275f42cf20723015747dace66d1d733ba8
KayqueAlves <kayqueteixeira.a@gmail.com>
2023-02-11 16:50:42 -0300

    fix

--- JS/clock.js
+++ JS/clock.js
@@ -19,18 +19,19 @@ setInterval(() =>{
     minuteHand.style.transform = `translate(0, -50%) rotate(${minutes * 6}deg)`;
     hourHand.style.transform = `translate(0, -50%) rotate(${hours * 30}deg)`;
 
-    if (hours => 6 && hours <= 12) {
+    if (hours >= 6 && hours <= 12) {
         document.body.style.backgroundColor = "rgb(114, 214, 242)";
     }
-    if (hours => 12 && hours <= 18) {
+    if (hours >= 12 && hours <= 18) {
         document.body.style.backgroundColor = "rgb(248, 155, 78)";
     }
-    if (hours => 18 && hours <= 0) {
+    if (hours >= 18 && hours <= 23) {
         document.body.style.backgroundColor = "rgb(28, 51, 81)";
     }
-    if (hours => 0 && hours <= 6) {
+    if (hours >= 1 && hours <= 6) {
         document.body.style.backgroundColor = "rgb(14, 30, 52)";
-    }
+    }  
+
 
 }, 1000);
 
